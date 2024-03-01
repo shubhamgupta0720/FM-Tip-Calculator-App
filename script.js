38b797f89c3bd1c276f81070d5748e5c47d78bb5
Shubham <shubhamgupta0720@gmail.com>
2024-03-01 09:43:51 +0530

    Bug Fixes

--- script.js
+++ script.js
@@ -53,9 +53,38 @@ tipButtons.forEach(button => {
   });
 });
 
-customTipInput.addEventListener('input', () => {
+numOfPeople.addEventListener('input', () => {
   const tipValue = customTipInput.value;
   if(numOfPeople.value == "" || numOfPeople.value == "0"){
+    errorMessage.style.display = "block";
+    numOfPeople.style.border = "1px solid";
+    numOfPeople.style.borderColor = "rgb(250, 94, 94)";
+    resetButton.classList.remove('active');
+    resetButton.classList.add('disabled');
+  }
+  else if((numOfPeople.value != "" || numOfPeople.value != "0") && tipValue.value != ""){
+    errorMessage.style.display = "none";
+    numOfPeople.style.border = "none";
+    var totalTipPerPerson = (Number(billAmount.value) * (Number(tipValue) / 100)) / Number(numOfPeople.value);
+    console.log(totalTipPerPerson);
+    var totalBillPerPerson = (Number(billAmount.value) / Number(numOfPeople.value)) + Number(totalTipPerPerson);
+    console.log(totalBillPerPerson);
+    document.getElementById("tipDisplay").value = "$" + String(totalTipPerPerson.toFixed(2));
+    document.getElementById("totalDisplay").value = "$" + String(totalBillPerPerson.toFixed(2));
+    resetButton.classList.remove('disabled');
+    resetButton.classList.add('active');
+  }
+})
+
+customTipInput.addEventListener('input', () => {
+  const tipValue = customTipInput.value;
+  tipButtons.forEach(allButton => {
+    allButton.style.backgroundColor = "hsl(183, 100%, 15%)";
+    allButton.style.color = "white";
+  });
+  if(numOfPeople.value == "" || numOfPeople.value == "0"){
+    document.getElementById("tipDisplay").value = "";
+    document.getElementById("totalDisplay").value = "";
     errorMessage.style.display = "block";
     numOfPeople.style.border = "1px solid";
     numOfPeople.style.borderColor = "rgb(250, 94, 94)";
